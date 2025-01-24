e7ee4ec169c0a93cf6707befbe94f2b2fd3fe832
Kayusha <gulkaiyrnamazbek@gmail.com>
2025-01-24 13:31:26 +0000

    provided import commands

--- Homework2/main.tf
+++ Homework2/main.tf
@@ -4,62 +4,45 @@ provider aws {
  resource "aws_key_pair" "deployer" {
   key_name   = "Bastion-key"
   public_key = file("~/.ssh/id_rsa.pub") 
-
- }
+}
 
 resource "aws_s3_bucket" "example" {
   bucket_prefix = "kaizen-gulkaiyrnam-"
-  force_destroy =true 
+  force_destroy = true 
 }
 
- resource "aws_s3_bucket" "kaizen-kayu" {
+resource "aws_s3_bucket" "kaizen-kayu" {
   bucket = "kaizen-kayu"
-
-
 }
+
 resource "aws_s3_bucket" "kaizen-kayu2" {
   bucket = "kaizen-kayu2"
-
 }
 
 resource "aws_s3_bucket" "kaizen-kayu3" {
   bucket = "kaizen-kayu3"
-
 }
 
+resource "aws_s3_object" "object" {
+  depends_on = [aws_s3_bucket.example]
+  bucket     = aws_s3_bucket.example.bucket
+  key        = "main.tf"
+  source     = "main.tf"
+}
 
- resource "aws_s3_object" "object" {
-    depends_on = [aws_s3_bucket.example]
-  bucket = aws_s3_bucket.example.bucket
-  key = "main.tf"
-  source = "main.tf"
-
- }
-
- resource "aws_iam_user" "lb" {
+resource "aws_iam_user" "lb" {
   for_each = toset(["jihuo", "sana", "momo", "dahyun"])
-  name = each.value
- }
+  name     = each.value
+}
+
 resource "aws_iam_group_membership" "team" {
   name = "tf-testing-group-membership"
-
   users = [
-
-   for i in aws_iam_user.lb : i.name 
+    for i in aws_iam_user.lb : i.name 
   ]
-
   group = aws_iam_group.group.name
 }
 
 resource "aws_iam_group" "group" {
   name = "twice"
 }
-
-
-
-
-
- 
-
-
- 
